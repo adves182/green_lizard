1a91a7f0fdcffcfbeb492d834c0092cbc676041c
adves182 <adves1313@uib.com>
2026-02-09 14:01:27 +0100

    added package ggplot2

--- penguins.ER.R
+++ penguins.ER.R
@@ -1,8 +1,11 @@
 library(palmerpenguins)
 library(tidyverse)
+library(dplyr)
+library(ggplot2)
 penguins %>% 
   count(species)
 penguins %>% 
   group_by(species) %>% 
   summarize(across(where(is.numeric), mean, na.rm = TRUE))
+
 
